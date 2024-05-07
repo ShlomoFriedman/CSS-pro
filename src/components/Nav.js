782b4b35f887e4fd4aacbadb908f347f0aa79fc3
ShlomoFriedman <123473216+ShlomoFriedman@users.noreply.github.com>
2024-05-07 16:13:12 +0300

    on the road to responsive css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    // State to toggle the hamburger menu
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    // Function to toggle the menu state
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className='navbar'>
             <img src="/Little%20Lemon%20Logo.svg" alt="Logo" />
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#order online">Order Online</a></li>
-            </ul>
-            <ul className='Login'>
-                <li ><a href="#login">Login</a></li>
-            </ul>
+
+            {/* Hamburger Button */}
+            <button className="hamburger" onClick={toggleMenu} aria-label="Toggle navigation">
+                ☰
+            </button>
+            
+
+            {/* Main Navigation Menu */}
+            <nav className={`navbar-menu ${menuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#order online">Order Online</a></li>
+                </ul>
+                <ul className='Login'>
+                    <li><a href="#login">Login</a></li>
+                </ul>
+            </nav>
         </header>
-    )
+    );
 }
 
 export default Nav;
